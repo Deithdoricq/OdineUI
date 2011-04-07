c687b912b5233e212be20d14d3282b9047cc1861
Odine <odine@duppa.org>
2011-04-06 21:26:02 -0400

    adjusted datatext panel to 11, fixed bug with remindbuffs lua error

---
+++ Tukui/config/filters/reminder.lua
@@ -0,0 +1,43 @@
+local T, C, L, DB = unpack(select(2, ...)) -- Import Functions/Constants, Config, Locales
+
+--------------------------------------------------------------------------------------------
+-- Spells that should be shown with an icon in the middle of the screen when not buffed.
+--------------------------------------------------------------------------------------------
+
+T.remindbuffs = {
+	PRIEST = {
+		588, -- inner fire
+		73413, -- inner will
+	},
+	HUNTER = {
+		13165, -- hawk
+		5118, -- cheetah
+		13159, -- pack
+		20043, -- wild
+		82661, -- fox
+	},
+	MAGE = {
+		7302, -- frost armor
+		6117, -- mage armor
+		30482, -- molten armor
+	},
+	WARLOCK = {
+		28176, -- fel armor
+		687, -- demon armor
+	},
+	SHAMAN = {
+		52127, -- water shield
+		324, -- lightning shield
+		974, -- earth shield
+	},
+	WARRIOR = {
+		469, -- commanding Shout
+		6673, -- battle Shout
+	},
+	DEATHKNIGHT = {
+		57330, -- horn of Winter
+		31634, -- strength of earth totem
+		6673, -- battle Shout
+		93435, -- roar of courage (hunter pet)
+	},
+}

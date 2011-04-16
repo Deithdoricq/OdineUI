aa57ecfe824c7ca05e330f6c3366e9cb76906065
Odine <odine@duppa.org>
2011-04-16 12:45:24 -0400

    more locale changes

---
+++ Tukui_Config/local/china.lua
@@ -0,0 +1,4 @@
+-- English localization file for enUS and enGB.
+local AceLocale = LibStub:GetLibrary("AceLocale-3.0")
+local L = AceLocale:NewLocale("OUI", "zhCN")
+if not L then return end
---
+++ Tukui_Config/local/spanish.lua
@@ -0,0 +1,4 @@
+-- English localization file for enUS and enGB.
+local AceLocale = LibStub:GetLibrary("AceLocale-3.0")
+local L = AceLocale:NewLocale("OUI", "esES") or AceLocale:NewLocale("OUI", "esMX")
+if not L then return end
